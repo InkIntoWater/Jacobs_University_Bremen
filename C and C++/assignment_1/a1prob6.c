836c122037a1c4b4e3065c0abb83a1fc2a2dc6a5
Suraj Giri <63389681+InkIntoWater@users.noreply.github.com>
2021-09-08 10:25:06 +0200

    Add files via upload

---
+++ C and C++/assignment_1/a1prob6.c
@@ -0,0 +1,15 @@
+/* 
+CH-230-A
+a1_p6.c
+Suraj Giri
+sgiri@jacobs-university.de
+*/
+
+#include<stdio.h>
+int main()
+{
+    char c= 'F';
+    c+=3;
+    printf("Character within alphabet is %c \n", c);
+    printf("Corresponding ASCII Code = %d \n ", c);
+}
